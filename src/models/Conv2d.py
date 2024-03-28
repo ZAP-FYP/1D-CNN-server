505abd68cfd45d022cf535c67cb96e8ba26e2888
springyboiii <sugumararvinth19991018@gmail.com>
2024-03-28 21:29:15 +0530

    pooling

--- src/models/Conv2d.py
+++ src/models/Conv2d.py
@@ -125,17 +125,12 @@ class DeepConv2d_Residual(nn.Module):
         
         return x8.view(x8.size(0), 5, 168, 256)
 
-class DeepConv2d_Pooling(nn.Module):
+class Conv2d_Pooling(nn.Module):
     def __init__(self):
-        super(DeepConv2d_Pooling, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=10, out_channels=32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1)
-        self.conv6 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
-        self.conv7 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-        self.conv8 = nn.Conv2d(in_channels=32, out_channels=5, kernel_size=3, padding=1)
+        super(Conv2d_Pooling, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=10, out_channels=16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=5, kernel_size=3, padding=1)
         
         self.sigmoid = nn.Sigmoid()
         self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2)
@@ -151,25 +146,6 @@ class DeepConv2d_Pooling(nn.Module):
         
         x = torch.relu(self.conv3(x))
         x = self.avg_pool(x)  # Apply average pooling layer
-
-        
-        x = torch.relu(self.conv4(x))
-        x = self.avg_pool(x)  # Apply average pooling layer
-
-        
-        x = torch.relu(self.conv5(x))
-        x = self.avg_pool(x)  # Apply average pooling layer
-
-        
-        x = torch.relu(self.conv6(x))
-        x = self.avg_pool(x)  # Apply average pooling layer
-
-        
-        x = torch.relu(self.conv7(x))
-        x = self.avg_pool(x)  # Apply average pooling layer
-
-        
-        x = torch.relu(self.conv8(x))
         
         x = self.sigmoid(x)  # Applying sigmoid activation
 
