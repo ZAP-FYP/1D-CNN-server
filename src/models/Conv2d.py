81964ab62f46f5a729d2fdf4ab04b107aa6bdc42
springyboiii <sugumararvinth19991018@gmail.com>
2024-03-28 20:04:57 +0530

    reduced layers

--- src/models/Conv2d.py
+++ src/models/Conv2d.py
@@ -57,11 +57,9 @@ class DeepConv2d(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=10, out_channels=32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1)
-        self.conv6 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
-        self.conv7 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
-        self.conv8 = nn.Conv2d(in_channels=32, out_channels=5, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
+        self.conv6 = nn.Conv2d(in_channels=32, out_channels=5, kernel_size=3, padding=1)
         
         self.sigmoid = nn.Sigmoid()
 
@@ -72,8 +70,6 @@ class DeepConv2d(nn.Module):
         x = torch.relu(self.conv4(x))
         x = torch.relu(self.conv5(x))
         x = torch.relu(self.conv6(x))
-        x = torch.relu(self.conv7(x))
-        x = torch.relu(self.conv8(x))
         
         x = self.sigmoid(x)
         
