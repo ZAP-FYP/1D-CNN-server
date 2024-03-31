f5895648cda9be303509746f3795c25d8c47e4ac
springyboiii <sugumararvinth19991018@gmail.com>
2024-03-31 13:47:37 +0530

    a

--- src/models/Conv2d.py
+++ src/models/Conv2d.py
@@ -8,22 +8,52 @@ class Conv2d(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=10, out_channels=16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=5, kernel_size=3, padding=1)
-        # self.pool = nn.MaxPool2d(kernel_size=2, stride=2)  # Max pooling layer
         self.sigmoid = nn.Sigmoid()  # Sigmoid activation layer
 
-        # self.fc = nn.Linear(5 * (168//8 ) * (256//8 ), 1152000)  
 
     def forward(self, x):
         x = torch.relu(self.conv1(x))
-        # x = self.pool(x)  # Apply pooling layer
         x = torch.relu(self.conv2(x))
-        # x = self.pool(x)  # Apply pooling layer
         x = torch.relu(self.conv3(x))
 
         x = self.sigmoid(x)  # Applying sigmoid activation
         
         return x.view(x.size(0), 5, 168, 256)
 
+class Conv2dLSTM(nn.Module):
+    def __init__(self):
+        super(Conv2dLSTM, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=10, out_channels=16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=5, kernel_size=3, padding=1)
+        self.sigmoid = nn.Sigmoid()  # Sigmoid activation layer
+
+
+        # Define LSTM layers
+
+        self.lstm = nn.LSTM(input_size=5*168*256, hidden_size=5*256, num_layers=2, batch_first=True)
+        self.fc = nn.Linear(5 * 256, 64*5*168*256)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = torch.relu(self.conv3(x))
+
+        # Reshape x for LSTM
+
+        x = x.view(x.size(0), -1)
+        print(f'inpit lstm {x.unsqueeze(1).shape}')
+        # LSTM layer
+
+        out, _ = self.lstm(x.unsqueeze(1))  # Add a channel dimension for LSTM
+
+        print(f'output lstm {out.shape}')
+
+        # Sigmoid activation
+        x = self.fc(out)
+        x = self.sigmoid(x)
+
+        return x.view(x.size(0), 5, 168, 256)  
 
 
 class Conv2d_Residual(nn.Module):
@@ -180,19 +210,19 @@ class Conv2d_Pooling_Deconv(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)  # Add batch normalization after conv1
 
         self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2, padding=1)
-        self.deconv1 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4, stride=2, padding=1)
+        self.deconv1 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=4,stride = 2 , padding = 2)
 
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=128, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(128)  # Add batch normalization after conv2
 
         self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2, padding=1)
-        self.deconv2 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4,stride=2, padding=1 )
+        self.deconv2 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=4,stride=2, padding=2 )
 
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(32)  # Add batch normalization after conv3
 
         self.avg_pool = nn.AvgPool2d(kernel_size=2, stride=2, padding=1)
-        self.deconv3 = nn.ConvTranspose2d(in_channels=32, out_channels=5, kernel_size=4,stride=2, padding=1 )
+        self.deconv3 = nn.ConvTranspose2d(in_channels=32, out_channels=5, kernel_size=4,stride=2, padding=2 )
 
         self.sigmoid = nn.Sigmoid()
 
@@ -201,19 +231,20 @@ class Conv2d_Pooling_Deconv(nn.Module):
 
         x = self.avg_pool(x)
         x = torch.relu(self.deconv1(x))
-        print(f'after 1rd deconv {x.shape}')
+        # print(f'after 1rd deconv {x.shape}')
 
         x = torch.relu(self.bn2(self.conv2(x)))  # Apply batch normalization before activation
 
         x = self.avg_pool(x)
         x = torch.relu(self.deconv2(x))
-        print(f'after 2rd deconv {x.shape}')
+        # print(f'after 2rd deconv {x.shape}')
 
         x = torch.relu(self.bn3(self.conv3(x)))  # Apply batch normalization before activation
 
         x = self.avg_pool(x)
         x = torch.relu(self.deconv3(x))
-        print(f'after 3rd deconv {x.shape}')
+        # print(f'after 3rd deconv {x.shape}')
         x = self.sigmoid(x)
 
         return x.view(x.size(0), 5, 168, 256)
+
