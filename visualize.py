04efea4d3b30e76be0661cddfd499f8115637f24
springyboiii <sugumararvinth19991018@gmail.com>
2024-04-23 11:19:32 +0530

    a

--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-def visualize_frames(video_data, output_dir, num_frames=15, frame_rate=30, total_window=3):
+def visualize_frames(video_data, video_data2, output_dir, num_frames=15, frame_rate=30, total_window=3):
     total_frames = video_data.shape[0]
     rolling_window = total_window*frame_rate
     interval = rolling_window//num_frames
@@ -13,6 +13,8 @@ def visualize_frames(video_data, output_dir, num_frames=15, frame_rate=30, total
     # if not os.path.exists(output_dir):
     #     os.makedirs(output_dir)
     selected_frames = [ video_data[i * interval] for i in range(total_frames//interval)]
+    selected_frames2 = [ video_data2[i * interval] for i in range(total_frames//interval)]
+
     print(len(selected_frames))
     # for idx in range(data_points):
         
@@ -24,7 +26,7 @@ def visualize_frames(video_data, output_dir, num_frames=15, frame_rate=30, total
 
         # Plotting
     for i in range(data_points):  # Loop over each sample in the batch
-        fig, axes = plt.subplots(3, 5, figsize=(15, 9))  # 3 rows, 5 columns for each sample
+        fig, axes = plt.subplots(3, 10, figsize=(15, 9))  # 3 rows, 5 columns for each sample
 
         # Plot images from batch_x for the current sample
         for j in range(5):
@@ -32,17 +34,25 @@ def visualize_frames(video_data, output_dir, num_frames=15, frame_rate=30, total
             axes[0, j].imshow(selected_frames[i*num_frames+j], cmap='gray')  # Assuming binary images (0 and 1)
             axes[0, j].set_title(f'True Label[{j} data point {i}]')
             axes[0, j].axis('off')
-
+            axes[0, j+5].imshow(selected_frames2[i*num_frames+j], cmap='gray')  # Assuming binary images (0 and 1)
+            axes[0, j+5].set_title(f'True Label[{j} data point {i}]')
+            axes[0, j+5].axis('off')
         for j in range(5,10):
             axes[1, j-5].imshow(selected_frames[i*num_frames+j], cmap='gray')  # Assuming binary images (0 and 1)
             axes[1, j-5].set_title(f'True Label[{j} data point {i}]')
             axes[1, j-5].axis('off')
+            axes[1, j].imshow(selected_frames2[i*num_frames+j], cmap='gray')  # Assuming binary images (0 and 1)
+            axes[1, j].set_title(f'True Label[{j} data point {i}]')
+            axes[1, j].axis('off')
         
         # Plot images from batch_y for the current sample
         for j in range(10,15):
             axes[2, j-10].imshow(selected_frames[i*num_frames+j], cmap='gray')  # Assuming binary images (0 and 1)
             axes[2, j-10].set_title(f'True Label[{j} data point {i}]')
             axes[2, j-10].axis('off')
+            axes[2, j-5].imshow(selected_frames2[i*num_frames+j], cmap='gray')  # Assuming binary images (0 and 1)
+            axes[2, j-5].set_title(f'True Label[{j} data point {i}]')
+            axes[2, j-5].axis('off')
 
         plt.tight_layout()
         # plt.suptitle(f"Loss: {loss.item():.4f}", fontsize=16)
@@ -57,4 +67,4 @@ video_data = np.load('/Users/springy/Desktop/New Projects/YOLOPv2-1D_Coordinates
 output_dir = 'visualizations'
 
 # Visualize and save frames
-visualize_frames(video_data, output_dir, num_frames=15, total_window=3)
+visualize_frames(video_data,video_data, output_dir, num_frames=15, total_window=3)
