4107f59a74c594c664dc0a31e99f6ecd952f2596
springyboiii <sugumararvinth19991018@gmail.com>
2024-05-08 16:06:57 +0530

    a

--- src/models/pretrained_model.py
+++ src/models/pretrained_model.py
@@ -22,25 +22,25 @@ class CollisionClassifier(nn.Module):
         # Convolutional LSTM layers
         self.conv_layer1 = nn.Sequential(
             nn.Conv1d(
-                in_channels=500, out_channels=500, kernel_size=5, stride=1, padding=2
+                in_channels=5, out_channels=5, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
         self.conv_layer2 = nn.Sequential(
             nn.Conv1d(
-                in_channels=500, out_channels=500, kernel_size=5, stride=1, padding=2
+                in_channels=5, out_channels=5, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
         self.conv_layer3 = nn.Sequential(
             nn.Conv1d(
-                in_channels=500, out_channels=100, kernel_size=5, stride=1, padding=2
+                in_channels=5, out_channels=5, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
         self.conv_layer4 = nn.Sequential(
             nn.Conv1d(
-                in_channels=100, out_channels=100, kernel_size=5, stride=1, padding=2
+                in_channels=5, out_channels=1, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
@@ -51,12 +51,15 @@ class CollisionClassifier(nn.Module):
     def forward(self, x):
         # Output of pre-trained model
         pretrained_output = self.pretrained_model(x)
-        
+        print(pretrained_output.shape)
+
+        pretrained_output = pretrained_output.view(pretrained_output.shape[0],5,100)
+        print(pretrained_output.shape)
         x = self.conv_layer1(pretrained_output)
         x = self.conv_layer2(x)
         x = self.conv_layer3(x)
         x = self.conv_layer4(x)
-        x = self.fc(x)
+        x = self.fc(x.squeeze(1))
 
         final_output = self.sigmoid(x)
         
@@ -108,7 +111,7 @@ def get_classification_model_full(pretrained_model1, checkpoint_file1,pretrained
     pretrained_model1.load_state_dict(checkpoint1["model_state_dict"])  
     checkpoint2 = torch.load(checkpoint_file2)
     # pretrained_model = checkpoint['model_state_dict']
-    pretrained_model2.load_state_dict(checkpoint2["model_state_dict"])  
+    pretrained_model2.load_state_dict(checkpoint2["model_state_dict"], strict=False)  
     print("pretrained_model1:\n", pretrained_model1)
     print("pretrained_model2:\n", pretrained_model2)
 
@@ -119,64 +122,79 @@ def get_classification_model_full(pretrained_model1, checkpoint_file1,pretrained
 class CollisionClassifierTrainable(nn.Module):
     def __init__(self):
         super(CollisionClassifierTrainable, self).__init__()
-
         self.conv_layer1 = nn.Sequential(
             nn.Conv1d(
-                in_channels=11, out_channels=11, kernel_size=3, stride=1, padding=2
+                in_channels=5, out_channels=5, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
         self.conv_layer2 = nn.Sequential(
             nn.Conv1d(
-                in_channels=11, out_channels=11, kernel_size=5, stride=1, padding=2
+                in_channels=5, out_channels=5, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
         self.conv_layer3 = nn.Sequential(
             nn.Conv1d(
-                in_channels=11, out_channels=11, kernel_size=7, stride=1, padding=2
+                in_channels=5, out_channels=5, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
         self.conv_layer4 = nn.Sequential(
             nn.Conv1d(
-                in_channels=11, out_channels=11, kernel_size=9, stride=1, padding=2
+                in_channels=5, out_channels=1, kernel_size=5, stride=1, padding=2
             ),
             nn.ReLU(),
         )
         # Replace fc layer in pretrained model with new classifier
-        self.fc1 = nn.Linear(11*96, 11*50)
-        self.fc2 = nn.Linear(11*50, 11*10)
-        self.fc3 = nn.Linear(11*10, 11*1)
-        self.fc4 = nn.Linear(11*1, 1)
-
+        self.fc = nn.Linear(100, 1)
         self.sigmoid = nn.Sigmoid()
-
 
     def forward(self, x):
         # Output of pre-trained model
         # pretrained_output = self.pretrained_model(x)
-        # pretrained_output = pretrained_output[:,-100:]
-        batch_size = x.shape[0]
-        reshaped_input = x.view(batch_size, 5, 100)
-
         # print(pretrained_output.shape)
-        # print(reshaped_input.shape)
-        # x =  x.view(batch_size,)
-        # print(x.shape, reshaped_input[:,-1,:].shape)
-        concatenated_tensor = torch.cat((x, reshaped_input[:,-1,:].unsqueeze(1)), dim=1)
-        # Output of the final classification layer
-        x = self.conv_layer1(concatenated_tensor)
+
+        pretrained_output = x.view(x.shape[0],5,100)
+        # print(pretrained_output.shape)
+        x = self.conv_layer1(pretrained_output)
         x = self.conv_layer2(x)
         x = self.conv_layer3(x)
         x = self.conv_layer4(x)
-        # print(f' shape before fc {x.shape,x.squeeze().shape}')
-        x = self.fc1(x.view(batch_size,-1))
-        # print(f' shape after fc {x.shape}')
-        x = self.fc2(x)
-        x = self.fc3(x)
-        x = self.fc4(x)
+        x = self.fc(x.squeeze(1))
 
         final_output = self.sigmoid(x)
         
         return final_output
+
+        
+
+
+    # def forward(self, x):
+    #     # Output of pre-trained model
+    #     # pretrained_output = self.pretrained_model(x)
+    #     # pretrained_output = pretrained_output[:,-100:]
+    #     batch_size = x.shape[0]
+    #     reshaped_input = x.view(batch_size, 5, 100)
+
+    #     # print(pretrained_output.shape)
+    #     # print(reshaped_input.shape)
+    #     # x =  x.view(batch_size,)
+    #     print(x.shape, reshaped_input[:,-1,:].shape)
+    #     concatenated_tensor = torch.cat((x, reshaped_input[:,-1,:]), dim=0)
+    #     # print(concatenated_tensor.shape)
+    #     # Output of the final classification layer
+    #     x = self.conv_layer1(concatenated_tensor)
+    #     x = self.conv_layer2(x)
+    #     x = self.conv_layer3(x)
+    #     x = self.conv_layer4(x)
+    #     # print(f' shape before fc {x.shape,x.squeeze().shape}')
+    #     x = self.fc1(x.view(batch_size,-1))
+    #     # print(f' shape after fc {x.shape}')
+    #     x = self.fc2(x)
+    #     x = self.fc3(x)
+    #     x = self.fc4(x)
+
+    #     final_output = self.sigmoid(x)
+        
+    #     return final_output
